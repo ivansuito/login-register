b0efe460cfd861d476f6d00770bf99adc39872c4
Ivan <64205736+ivansuito@users.noreply.github.com>
2022-04-05 12:34:54 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+bool IsLogged() 
+{
+    string username, password, us, pw;
+
+    cout << "Nombre de usuario: "; cin >> username;
+    cout << "Pasword: "; cin >> password;
+
+    ifstream read("C:/Users/eldel/OneDrive/Escritorio/login-register/" +  username + ".txt");
+    getline(read, us);
+    getline(read, pw);
+
+    if (us == username && pw == password){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() 
+{
+    int q1;
+
+    cout << "1)Registrarse.\n2)Iniciar sesion.\nElige: "; cin >> q1;
+
+    if (q1 == 1)
+    {
+        string username, password;
+        cout << "Nombre de usuario: "; cin >> username;
+        cout << "Password: "; cin >> password;
+
+        ofstream file;
+        file.open("C:/Users/eldel/OneDrive/Escritorio/login-register/" + username + ".txt");
+        file << username << endl << password; 
+        file.close();
+
+        main();
+    }
+
+    else if (q1 == 2)
+    {
+        bool status = IsLogged(); 
+
+        if (!status)
+        {
+            cout << "No existe esta cuenta." << endl;
+            system("PAUSE");
+            return 0;
+        } else 
+        {
+            cout << "Has iniciado sesion!" << endl;
+            system("PAUSE");
+            return 1;
+        }
+        
+    }
+    
+}
